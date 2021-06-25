cdcd9b19f6c67b9f600e9eda4d14b5c6e75dc6ea
jhock9 <jon.hocker@gmail.com>
2021-06-25 15:51:35 -0500

    working on fetching repo data

--- js/script.js
+++ js/script.js
@@ -1,29 +1,62 @@
 // Where profile info will appear
 const overview = document.querySelector(".overview");
 const username = "jhock9";
+const repoList = document.querySelector(".repo-list");
 
-const getUserInfo = async function () {
+const gitUserInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
-  const data = await userInfo.json();
-  // console.log(data);
-  displayUserInfo(data);
+  const userData = await userInfo.json();
+  // console.log(userData);
+  displayUserInfo(userData);
 }
 
-getUserInfo();
+gitUserInfo();
 
-const displayUserInfo = function (data) {
+const displayUserInfo = function (userData) {
   const div = document.createElement("div");
   div.classList.add("user-info");
   div.innerHTML = `
     <figure>
-      <img alt="user avatar" src=${data.avatar_url} />
+      <img alt="user avatar" src=${userData.avatar_url} />
     </figure>
     <div>
-      <p><strong>Name:</strong> ${data.name}</p>
-      <p><strong>Bio:</strong> ${data.bio}</p>
-      <p><strong>Location:</strong> ${data.location}</p>
-      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+      <p><strong>Name:</strong> ${userData.name}</p>
+      <p><strong>Bio:</strong> ${userData.bio}</p>
+      <p><strong>Location:</strong> ${userData.location}</p>
+      <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
     </div>
   `;
   overview.append(div);
 };
+
+const gitRepos = async function () {
+  const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=created&per_page=100`);
+  const repoData = await fetchRepos.json();
+  // console.log(repoData);
+  displayRepos(repoData);
+}
+
+gitRepos();
+
+const displayRepos = function (repos) {
+
+for (const repo of  )
+
+/*
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${userData.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${userData.name}</p>
+      <p><strong>Bio:</strong> ${userData.bio}</p>
+      <p><strong>Location:</strong> ${userData.location}</p>
+      <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
+    </div>
+  `;
+  repoList.append(div);
+*/
+};
+
